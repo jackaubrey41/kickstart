5bd7677db625eadc089b19b726043ef1144fae5e
jackaubrey41 <46295760+jackaubrey41@users.noreply.github.com>
2019-01-09 09:21:19 +0100

    Update kickstarv1jr.sol

--- kickstarv1jr.sol
+++ kickstarv1jr.sol
@@ -8,7 +8,7 @@ contract Kickstart {
         uint amount;                                    // Ether to transfer
         address recipient;                              // Who gets de money
         bool complete;                                  // Whether the request is done
-        mapping ( address => bool) public approvalsMap; // Track who has voted, 0x0 no voted, T or F voted.
+        mapping ( address => bool) approvalsMap;        // Track who has voted, 0x0 no voted, T or F voted.
         uint approvalCount;                             // Track number of approvals. Increase 1 with each approval.
     }
     
@@ -17,20 +17,22 @@ contract Kickstart {
     mapping ( uint => address ) public approversMap;    // list of addresses of contributors indexed
     Request[] public requeslist;                        // List of requests created by mnanager in this campaign. Dinamic array
 
-    uint private index;                                 // contributors and count index
+    uint private numContributors;                                 // contributors count index
+  //  uint private amountContribution;
 
-    constructor (uint _minimumContribution) public {     // Constructor Campaign minimum contribution is argument
-        manager = msg.sender;                            // who deploy is the manager
+    constructor (uint _minimumContribution) public {    // Constructor Campaign minimum contribution is argument
+        manager = msg.sender;                           // who deploy is the manager
         minimumContribution = _minimumContribution;     // Init minimumContribution internal varialble
-        index = 0                                       // Init index array contributors
+        numContributors = 0 ;                                     // Init index array contributors
     }
     
-    function contribute (uint _amountContribution) {
-        amountContribution = _amountContribution
-        approversMap[index] = msg.sender
-        index++;                                        // increase index array contributors
+    function contribute () public payable {             // Payable function
+        require ( msg.value >= minimumContribution );   // Require that contribution is equal or major to minimumContribution
+        numContributors++;                              // increase number of contributors and index of array
+        approversMap[numContributors] = msg.sender;     // set @ in list of contributors
     }
     
-    function
-    
+    function getBalance() public view returns(uint) {   // retorna la suma de contribuciones al proyecto
+        return address(this).balance;                   // retorna el balance del smart contrcat. función this accede a SmartContract
+    }
 }
